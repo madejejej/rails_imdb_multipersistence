0bdb803a8faf5f1244f4339b27baaff6b0c06ea0
root <akaterin@poczta.fm>
2014-06-22 19:45:48 +0200

    migration...

--- scripts/fromSqlMigration.rb
+++ scripts/fromSqlMigration.rb
@@ -1,26 +1,31 @@
-
+  @actors = Hash.new
   Actor.find_each do |actor|
-    $neo.create_node(fname: actor.fname, lname: actor.fname, mname: actor.mname, id: actor.idactors)
+    @node = $neo.create_node(fname: actor.fname, lname: actor.fname, mname: actor.mname)
+    @actors[actor.id] = @node
     $neo.add_label(actor.id, 'Actor')
   end
 
   puts "Actor batch saved!"
-
+  @movies = Hash.new
   Movie.find_each do |movie|
-    $neo.create_node(title: movie.title, id: movie.idmovies)
+    @node = $neo.create_node(title: movie.title, id: movie.idmovies)
+    @movies[movie.idmovies] = @node
     $neo.add_label(movie.id, 'Movie')
   end
 
   puts "Movie batch saved!"
 
+  @series = Hash.new
   Series.find_each do |series|
-    $neo.create_node(name: series.name, id: series.idseries)
+    @node = $neo.create_node(name: series.name, id: series.idseries)
+    @series[series.idseries] = @node
     $neo.add_label(series.id, 'Series')
   end
 
 
   ActedIn.find_each do |rel|
-    rel.neo_save
+    @actorNode = actors[rel.idactors]
+    
   end
 
   puts "ActedIn batch saved!"
