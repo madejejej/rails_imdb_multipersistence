3c36a5abac45b0496f0269dbd0ce3893e74ea662
Katarzyna Kurzynska <akaterin@poczta.fm>
2014-06-23 02:05:35 +0200

    Basic interface

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -30,6 +30,20 @@ class Actor < ActiveRecord::Base
     idactors
   end
 
+  def findRecomendations
+    query = "MATCH
+    (keanu:Actor {fname:" + fname +", lname:" + lname + "})-[:ACTED_IN]->()<-[:ACTED_IN]-(c),
+        (c)-[:ACTED_IN]->()<-[:ACTED_IN]-(coc)
+    WHERE coc <> keanu  AND NOT((keanu)-[:ACTED_IN]->()<-[:ACTED_IN]-(coc))
+    RETURN coc.name, count(coc)
+    ORDER BY count(coc) DESC;"
+    return @neo.execute_query(query)
+  end
+
+  def moviesBeforeYear(year)
+      return ActedIn.select("count(*)").join(:actors, :movies).where("actors.fname :fname and actors.lname = :lane and  movies.year" < year, {fname: fname, lname: lname})
+  end
+
   if ActiveRecord::VERSION::STRING < '4.0.0' || defined?(ProtectedAttributes)
     attr_accessible :idactors, :lname, :fname, :mname, :gender, :number
   end
